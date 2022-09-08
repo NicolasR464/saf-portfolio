9f273fee3fc9fd318af793fb814b96ba63ec83ea
niko <nicolas.rocagel@gmail.com>
2022-09-08 19:26:29 +0200

    add link index page

--- public/js/main.js
+++ public/js/main.js
@@ -7,8 +7,6 @@ const burgerLinks = document.querySelectorAll(".burger--link");
 const linkIndexed = document.querySelector(".burger-links");
 const listWrapper = document.querySelector(".burger-link-cont");
 
-const loadWrap = document.querySelector(".loader-wrapper");
-console.log(loadWrap);
 // addEventListener("load", () => {
 //   console.log("page loaded");
 //   // loadWrap.classList.add("wrapper-off");
@@ -27,12 +25,12 @@ console.log(loadWrap);
 //   }, 2030);
 // });
 
-// burgerWrap.addEventListener("click", () => {
-//   burger.classList.toggle("tapped");
-//   burgerWrap.classList.toggle("tapped");
-//   wrapBlur.classList.toggle("blur");
-//   burgerLinks.forEach((el) => {
-//     el.classList.toggle("showlink");
-//   });
-//   linkIndexed.classList.toggle("indexed");
-// });
+burgerWrap.addEventListener("click", () => {
+  burger.classList.toggle("tapped");
+  burgerWrap.classList.toggle("tapped");
+  wrapBlur.classList.toggle("blur");
+  burgerLinks.forEach((el) => {
+    el.classList.toggle("showlink");
+  });
+  linkIndexed.classList.toggle("indexed");
+});
