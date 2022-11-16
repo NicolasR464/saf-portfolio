be4e2fb5be1c5e9bcf6cebad5e89c60ba72bcf9a
niko <nicolas.rocagel@gmail.com>
2022-11-16 15:47:08 +0100

    fix CSP#3

--- app.js
+++ app.js
@@ -27,50 +27,50 @@ app.use(device.capture());
 const adminRoutes = require("./routes/admin");
 const portfolioRoutes = require("./routes/portfolio");
 //
-//app.use(helmet());
-app.use(
-  helmet.contentSecurityPolicy({
-    directives: {
-      defaultSrc: [
-        "'self'",
-        "https://res.cloudinary.com/niikkoo/image/upload/c_fill,f_auto,q_auto/v1/saf_portfolio/index/xf2vsxsgbof9hfrkqdfy",
-        "https://res.cloudinary.com/niikkoo/image/upload/c_fill,f_auto,h_949,q_auto,w_534,x_644,y_15/v1/saf_portfolio/index/xf2vsxsgbof9hfrkqdfy",
-      ],
+app.use(helmet());
+// app.use(
+//   helmet.contentSecurityPolicy({
+//     directives: {
+//       defaultSrc: [
+//         "'self'",
+//         "https://res.cloudinary.com/niikkoo/image/upload/c_fill,f_auto,q_auto/v1/saf_portfolio/index/xf2vsxsgbof9hfrkqdfy",
+//         "https://res.cloudinary.com/niikkoo/image/upload/c_fill,f_auto,h_949,q_auto,w_534,x_644,y_15/v1/saf_portfolio/index/xf2vsxsgbof9hfrkqdfy",
+//       ],
 
-      scriptSrc: [
-        "'self'",
-        "player.vimeo.com/api/player.js",
-        "cdn.jsdelivr.net/npm/swiper@8/swiper-bundle.min.js",
-        "https://fonts.googleapis.com",
-        "https://fonts.gstatic.com",
-        "cdnjs.cloudflare.com/ajax/libs/cropperjs/1.5.6/cropper.min.js",
-        "kit.fontawesome.com/766e633129.js",
-        "youtube.com",
-        "vimeo.com",
-        "youtube.com/iframe_api",
-        "res.cloudinary.com",
-        "https://fonts.googleapis.com/css2?family=Spartan:wght@200&display=swap",
-        "https://fonts.googleapis.com/css2?family=Poiret+One&display=swap",
-        "https://res.cloudinary.com/niikkoo/",
-        "https://res.cloudinary.com/niikkoo/image/upload/c_fill,f_auto,h_949,q_auto,w_534,x_644,y_15/v1/saf_portfolio/index/xf2vsxsgbof9hfrkqdfy",
-        "https://res.cloudinary.com/niikkoo/image/upload/c_fill,f_auto,q_auto/v1/saf_portfolio/index/xf2vsxsgbof9hfrkqdfy",
-      ],
-      styleSrc: [
-        "'self'",
-        "cdn.jsdelivr.net/npm/swiper@8/swiper-bundle.min.css",
-        "cdnjs.cloudflare.com/ajax/libs/cropperjs/1.5.6/cropper.min.css",
-      ],
-      fontSrc: [
-        "https://fonts.googleapis.com/css2?family=Spartan:wght@200&display=swap",
-        "https://fonts.googleapis.com/css2?family=Poiret+One&display=swap",
-      ],
-      imgSrc: ["'self'", "cloudinary.com"],
-      reportUri: "/report-violation",
-      objectSrc: [],
-    },
-    reportOnly: false,
-  })
-);
+//       scriptSrc: [
+//         "'self'",
+//         "player.vimeo.com/api/player.js",
+//         "cdn.jsdelivr.net/npm/swiper@8/swiper-bundle.min.js",
+//         "https://fonts.googleapis.com",
+//         "https://fonts.gstatic.com",
+//         "cdnjs.cloudflare.com/ajax/libs/cropperjs/1.5.6/cropper.min.js",
+//         "kit.fontawesome.com/766e633129.js",
+//         "youtube.com",
+//         "vimeo.com",
+//         "youtube.com/iframe_api",
+//         "res.cloudinary.com",
+//         "https://fonts.googleapis.com/css2?family=Spartan:wght@200&display=swap",
+//         "https://fonts.googleapis.com/css2?family=Poiret+One&display=swap",
+//         "https://res.cloudinary.com/niikkoo/",
+//         "https://res.cloudinary.com/niikkoo/image/upload/c_fill,f_auto,h_949,q_auto,w_534,x_644,y_15/v1/saf_portfolio/index/xf2vsxsgbof9hfrkqdfy",
+//         "https://res.cloudinary.com/niikkoo/image/upload/c_fill,f_auto,q_auto/v1/saf_portfolio/index/xf2vsxsgbof9hfrkqdfy",
+//       ],
+//       styleSrc: [
+//         "'self'",
+//         "cdn.jsdelivr.net/npm/swiper@8/swiper-bundle.min.css",
+//         "cdnjs.cloudflare.com/ajax/libs/cropperjs/1.5.6/cropper.min.css",
+//       ],
+//       fontSrc: [
+//         "https://fonts.googleapis.com/css2?family=Spartan:wght@200&display=swap",
+//         "https://fonts.googleapis.com/css2?family=Poiret+One&display=swap",
+//       ],
+//       imgSrc: ["'self'", "cloudinary.com"],
+//       reportUri: "/report-violation",
+//       objectSrc: [],
+//     },
+//     reportOnly: false,
+//   })
+// );
 
 app.use(compression());
 
