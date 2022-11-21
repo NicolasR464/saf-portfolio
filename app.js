12778cdf5e7cf4d2958a44be2c326575414f1133
niko <nicolas.rocagel@gmail.com>
2022-11-21 22:36:01 +0100

    add content security policy

--- app.js
+++ app.js
@@ -8,6 +8,55 @@ const device = require("express-device");
 const compression = require("compression");
 const helmet = require("helmet");
 
+app.use(
+  helmet.contentSecurityPolicy({
+    directives: {
+      defaultSrc: [
+        "'self'",
+        "cdn.jsdelivr.net",
+        "youtube.com/iframe_api",
+        "*.vimeo.com",
+        "vimeo.com",
+        "www.youtube.com",
+        "i.vimeocdn.com",
+        "i.ytimg.com",
+        "kit.fontawesome.com/",
+        "cdnjs.cloudflare.com",
+        "cloudflare.com",
+        "ka-f.fontawesome.com/",
+      ],
+      styleSrc: [
+        "'self'",
+        "cdn.jsdelivr.net",
+        "'unsafe-inline'",
+        "fonts.googleapis.com",
+        "cdnjs.cloudflare.com",
+      ],
+      fontSrc: [
+        "'self'",
+        "data:",
+        "fonts.gstatic.com",
+        "ka-f.fontawesome.com/",
+      ],
+      imgSrc: ["'self'", "blob:", "data:", "gap:", "res.cloudinary.com"],
+    },
+    reportOnly: false,
+  })
+);
+
+// scriptSrc: ["'self'", "youtube.com/iframe_api"],
+// frameAncestors: ["youtube.com", "vimeo.com", "youtube.com/iframe_api"],
+// formAction: ["'self'"],
+// baseUri: ["'self", "instagram.com"],
+// styleSrc: [
+//   "'self'",
+//   "cdn.jsdelivr.net/npm/swiper@8/swiper-bundle.min.css",
+// ],
+// fontSrc: ["'self'"],
+// imgSrc: ["'self'"],
+// reportUri: "/report-violation",
+// objectSrc: [],
+
 const session = require("express-session");
 const MongoDBStore = require("connect-mongodb-session")(session);
 const store = new MongoDBStore({
