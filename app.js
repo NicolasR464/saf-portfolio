aeb3b2dce18bef521c0a389a8acf3e3f0aba0330
niiko <nicolas.rocagel@gmail.com>
2025-11-29 19:01:25 +0100

    add banner

--- app.js
+++ app.js
@@ -1,105 +1,111 @@
-const express = require("express");
-const bodyParser = require("body-parser");
-const mongoose = require("mongoose");
-require("dotenv/config");
-const app = express();
-const path = require("path");
-const device = require("express-device");
-const compression = require("compression");
-const helmet = require("helmet");
+const express = require('express')
+const bodyParser = require('body-parser')
+const mongoose = require('mongoose')
+require('dotenv/config')
+const app = express()
+const path = require('path')
+const device = require('express-device')
+const compression = require('compression')
+const helmet = require('helmet')
+
+const portfolioContr = require('./controllers/portfolio')
 
 app.use(
-  helmet.contentSecurityPolicy({
-    directives: {
-      defaultSrc: [
-        "'self'",
-        "cdn.jsdelivr.net",
-        "youtube.com/iframe_api",
-        "*.vimeo.com",
-        "vimeo.com",
-        "www.youtube.com",
-        "i.vimeocdn.com",
-        "i.ytimg.com",
-        "kit.fontawesome.com/",
-        "cdnjs.cloudflare.com",
-        "cloudflare.com",
-        "ka-f.fontawesome.com/",
-      ],
-      styleSrc: [
-        "'self'",
-        "cdn.jsdelivr.net",
-        "'unsafe-inline'",
-        "fonts.googleapis.com",
-        "cdnjs.cloudflare.com",
-      ],
-      fontSrc: [
-        "'self'",
-        "data:",
-        "fonts.gstatic.com",
-        "ka-f.fontawesome.com/",
-      ],
-      imgSrc: ["'self'", "blob:", "data:", "gap:", "res.cloudinary.com"],
-    },
-    reportOnly: false,
-  })
-);
+    helmet.contentSecurityPolicy({
+        directives: {
+            defaultSrc: [
+                "'self'",
+                'cdn.jsdelivr.net',
+                'youtube.com/iframe_api',
+                '*.vimeo.com',
+                'vimeo.com',
+                'www.youtube.com',
+                'i.vimeocdn.com',
+                'i.ytimg.com',
+                'kit.fontawesome.com/',
+                'cdnjs.cloudflare.com',
+                'cloudflare.com',
+                'ka-f.fontawesome.com/',
+            ],
+            styleSrc: [
+                "'self'",
+                'cdn.jsdelivr.net',
+                "'unsafe-inline'",
+                'fonts.googleapis.com',
+                'cdnjs.cloudflare.com',
+            ],
+            fontSrc: [
+                "'self'",
+                'data:',
+                'fonts.gstatic.com',
+                'ka-f.fontawesome.com/',
+            ],
+            imgSrc: ["'self'", 'blob:', 'data:', 'gap:', 'res.cloudinary.com'],
+        },
+        reportOnly: false,
+    })
+)
 
-const session = require("express-session");
-const MongoDBStore = require("connect-mongodb-session")(session);
+const session = require('express-session')
+const MongoDBStore = require('connect-mongodb-session')(session)
 const store = new MongoDBStore({
-  uri: process.env.MONGO_URL,
-  collection: "sessions",
-  expires: 1000 * 60 * 60 * 24 * 30,
-});
-const flash = require("connect-flash");
+    uri: process.env.MONGO_URL,
+    collection: 'sessions',
+    expires: 1000 * 60 * 60 * 24 * 30,
+})
+const flash = require('connect-flash')
 //
 
-app.set("view engine", "ejs");
-app.set("views", "views");
+app.set('view engine', 'ejs')
+app.set('views', 'views')
 //
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(device.capture());
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(device.capture())
 //
-const adminRoutes = require("./routes/admin");
-const portfolioRoutes = require("./routes/portfolio");
+const adminRoutes = require('./routes/admin')
+const portfolioRoutes = require('./routes/portfolio')
 //
-app.use(helmet());
+app.use(helmet())
 
-app.use(compression());
+app.use(compression())
 
 app.use(
-  session({
-    secret: process.env.SESSION_SECRET,
-    resave: false,
-    saveUninitialized: false,
-    store: store,
-    cookie: {
-      maxAge: 1000 * 60 * 60 * 24 * 30,
-    },
-  })
-);
+    session({
+        secret: process.env.SESSION_SECRET,
+        resave: false,
+        saveUninitialized: false,
+        store: store,
+        cookie: {
+            maxAge: 1000 * 60 * 60 * 24 * 30,
+        },
+    })
+)
 
-app.use(flash());
+app.use(flash())
 //
-app.use(express.static(path.join(__dirname, "public")));
+app.use(express.static(path.join(__dirname, 'public')))
 
-app.use("/admin", adminRoutes);
+// 🚧
+app.use('*', portfolioContr.getIndex)
 
-app.use(portfolioRoutes);
+// Routes
+app.use('/admin', adminRoutes)
 
-app.use((req, res) => {
-  res.redirect("/");
-});
-//
+app.use(portfolioRoutes)
+
+app.use((_req, res) => {
+    res.redirect('/')
+})
+
 mongoose
-  .connect(process.env.MONGO_URL, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  })
-  .then(() => {
-    console.log("http://localhost:" + process.env.PORT, " 🚀");
-    app.listen(process.env.PORT || 5500);
-  })
-  .catch((err) => {
-    console.log(err);
-  });
+    .connect(process.env.MONGO_URL, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+    })
+    .then(() => {
+        console.log('http://localhost:' + process.env.PORT, ' 🚀')
+        app.listen(process.env.PORT || 5500)
+    })
+    .catch((err) => {
+        console.log(err)
+    })
--- routes/portfolio.js
+++ routes/portfolio.js
@@ -1,24 +1,23 @@
-const express = require("express");
-const router = express.Router();
+const express = require('express')
+const router = express.Router()
 
-const portfolioContr = require("../controllers/portfolio");
-// const portfolioVids = require("../models/portfolio-vids");
+const portfolioContr = require('../controllers/portfolio')
 
-router.get("/", portfolioContr.getIndex);
-router.get("/home/:orientation", portfolioContr.getIndex);
-router.get("/about", portfolioContr.getAbout);
-router.get("/portfolio", portfolioContr.getPortfolio);
-router.get("/contact", portfolioContr.getContact);
-router.post("/contact", portfolioContr.postContact);
-router.get("/thanks", (req, res, next) => {
-  res.render("portfolio/thanks", {
-    pageTitle: "Thanks",
-    path: "/",
-  });
-});
-router.get("/error", (req, res, next) => {
-  res.render("portfolio/error", {
-    pageTitle: "error",
-  });
-});
-module.exports = router;
+router.get('/', portfolioContr.getIndex)
+router.get('/home/:orientation', portfolioContr.getIndex)
+router.get('/about', portfolioContr.getAbout)
+router.get('/portfolio', portfolioContr.getPortfolio)
+router.get('/contact', portfolioContr.getContact)
+router.post('/contact', portfolioContr.postContact)
+router.get('/thanks', (_req, res) => {
+    res.render('portfolio/thanks', {
+        pageTitle: 'Thanks',
+        path: '/',
+    })
+})
+router.get('/error', (_req, res) => {
+    res.render('portfolio/error', {
+        pageTitle: 'error',
+    })
+})
+module.exports = router
